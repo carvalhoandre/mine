56799ae0d8eff3af93060715341d2bb8be9b1885
André Carvalho <andre_carvalho0@live.com>
2021-03-03 18:22:56 -0300

    :tada: Parâmetros do Jogo

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component () {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}> Tamanho da grade:
+          {params.gerRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+      </View>
+      
+    );
+  }
 }
 
 const styles = StyleSheet.create({

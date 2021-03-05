847fecee85b951e4896449ec1ccc555eb7e790a6
André Carvalho <andre_carvalho0@live.com>
2021-03-05 15:34:35 -0300

    :tada: Componente Field #01

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Field from './src/components/Field';
 import params from './src/params';
 
 export default class App extends Component () {
   render() {
     return (
       <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <StatusBar style="auto" />
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.instructions}> Tamanho da grade:
           {params.gerRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+
+        <Field />
       </View>
       
     );
